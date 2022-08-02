1d23ab7c6e6efcf656e338c3dc56febd71c22d76
Adilson Santana <100454561+AdilsonSS@users.noreply.github.com>
2022-08-02 13:05:47 -0300

    adicionado arquivo tabuada

---
+++ introducao_a_programacao/tabuada.py
@@ -0,0 +1,17 @@
+num = int(input('Digite um numero:'))
+print('-' * 12) ''' repete 12 vezes o traço'''
+# for x in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10):
+# print(s)
+# print(x)
+# s = s + x
+# print(n, '+ ', x, ' = ', s )
+print('{} x {:2} = {:2}'.format(num, 1, num * 1))
+print('{} x {:2} = {:2}'.format(num, 2, num * 2))
+print('{} x {:2} = {:2}'.format(num, 4, num * 4))
+print('{} x {:2} = {:2}'.format(num, 5, num * 5))
+print('{} x {:2} = {:2}'.format(num, 6, num * 6))
+print('{} x {:2} = {:2}'.format(num, 7, num * 7))
+print('{} x {:2} = {:2}'.format(num, 8, num * 8))
+print('{} x {:2} = {:2}'.format(num, 9, num * 9))
+print('{} x {:2} = {:2}'.format(num, 10, num * 10))
+print('-' * 12)
