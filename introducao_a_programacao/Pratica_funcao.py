4f442efbb4879cfa19e64ed6e84505b34c52fd37
Adilson Santana <100454561+AdilsonSS@users.noreply.github.com>
2022-08-02 19:56:34 -0300

    inserido arquivo pratica_funcao.py

---
+++ introducao_a_programacao/Pratica_funcao.py
@@ -0,0 +1,20 @@
+'''def print_alfa_num(abc_list, num_list):
+    for char in abc_list:
+        for num in num_list:
+            print(char, num)
+    return
+
+
+print_alfa_num(['a', 'b', 'c'], [1, 2, 3])'''
+
+
+'''def hello(meu_nome):
+    print('Olá,', meu_nome)
+
+hello("Adilson!")'''
+
+nomes = ['Pedro', 'João', 'Leticia']
+for n in nomes:
+     print(n)
+else:
+     print("Todos os nomes foram listados com sucesso")
